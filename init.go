26a1033092c55d9583377b8b422a408b114babe4
Tomtom <tomtom@herbesfolles.org>
2022-07-15 13:05:17 +0200

    les serveurs à coder

--- init.go
+++ init.go
@@ -1,5 +1,53 @@
 package main
 
+// serveur local du dirty district
+var dd = Server{}
+
+// serveur public du district 22
+var d22 = Server{}
+
+// serveur public de la kramps
+var kramps = Server{}
+
+// serveur privé de la kramps
+var kramps_priv = Server{}
+
+// serveur de sécurité de la kramps
+var kramps_sec = Server{}
+
+// serveur des services corporatistes D22
+var central = Server{}
+
+// serveur bancaire du D22
+var abus = Server{}
+
+// serveur public de Legba Voodoocom
+var legba = Server{}
+
+// serveur privé de la communication satellite
+var legba_satcom = Server{}
+
+// serveur archive de Silicon Spirit
+var legba_archive = Server{}
+
+// serveur le bon district
+var lbd = Server{}
+
+// green data
+var green = Server{}
+
+// serveur privé de Crunch
+var leet = Server{}
+
+// serveur privé de Céline
+var celine = Server{}
+
+// serveur mémoriel de Hope
+var hope = Server{}
+
+// fanpage The Black Wave
+var tbw = Server{}
+
 func (g Game) Init() error {
 	// nettoyer les tables
 	g.Drop(&Server{})

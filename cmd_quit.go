7c22750fecdeb326100c71e5979abaaa024f5237
Tomtom <tomtom@herbesfolles.org>
2022-07-17 11:41:54 +0200

    bugfix: quit déconnecte la DNI

--- cmd_quit.go
+++ cmd_quit.go
@@ -33,6 +33,7 @@ func (q Quit) Run(c *Client, args []string) tea.Msg {
 	c.Console.Login = ""
 	c.Console.Privilege = 0
 	c.Console.Alert = 0
+	c.Console.DNI = false
 
 	return ResultMsg{
 		Output: "déconnexion effectuée",

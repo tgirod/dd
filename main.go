f9ef4ffaa3209446e8e0ca1da6cbe45d8de0a5d3
Tomtom <tomtom@herbesfolles.org>
2023-03-28 22:35:01 +0200

    gofmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 )
 
 const host = "0.0.0.0"
@@ -11,13 +11,13 @@ const port = 1337
 func main() {
 	// Command line filename to init game with
 	args := os.Args
-	fmt.Printf( "args = %v \n",args )
-	fmt.Printf( "len(args)= %d \n", len(args) )
+	fmt.Printf("args = %v \n", args)
+	fmt.Printf("len(args)= %d \n", len(args))
 	a := NewApp()
 	if len(args) > 1 {
-		a.Start( args[1] )
+		a.Start(args[1])
 	} else {
-		a.Start( "" )
+		a.Start("")
 	}
 
 }

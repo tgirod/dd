ede4b482403796b2c062bfaef606af616de9c891
Tomtom <tomtom@herbesfolles.org>
2023-03-31 12:29:36 +0200

    commande pop dans un fichier séparé

---
+++ cmd_pop.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	lg "github.com/charmbracelet/lipgloss"
+	"github.com/muesli/reflow/wordwrap"
+)
+
+type Pop struct {
+	msg           string
+	width, height int
+}
+
+func (p *Pop) Init() tea.Cmd {
+	return nil
+}
+
+func (p *Pop) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		p.width = msg.Width
+		p.height = msg.Height
+	case tea.KeyMsg:
+		return p, func() tea.Msg { return CloseModalMsg{} }
+	}
+	return p, nil
+}
+
+var border = lg.NewStyle().Border(lg.DoubleBorder()).Padding(1)
+
+func (p *Pop) View() string {
+	return wordwrap.String(p.msg, p.width)
+}
+
+var pop = Cmd{
+	Name:      "pop",
+	ShortHelp: "ouvre une fenêtre modale",
+	Parse: func(args []string) any {
+		msg := strings.Join(args, " ")
+		if len(args) == 0 {
+			msg = "ceci est une fenêtre modale"
+		}
+		return OpenModalMsg(&Pop{msg: msg})
+	},
+}

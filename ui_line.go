e23bc6e20b113203db6fe757f2e3c41c3e37a88a
Tomtom <tomtom@herbesfolles.org>
2023-04-17 13:21:43 +0200

    aide pour ui_line

--- ui_line.go
+++ ui_line.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	bt_help "github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	lg "github.com/charmbracelet/lipgloss"
 )
 
+type LineKeymap struct {
+	Validate key.Binding
+	Cancel   key.Binding
+}
+
+var DefaultLineKeymap = LineKeymap{
+	Validate: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "valider"),
+	),
+	Cancel: key.NewBinding(
+		key.WithKeys("esc"),
+		key.WithHelp("esc", "annuler"),
+	),
+}
+
+func (k LineKeymap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Validate, k.Cancel}
+}
+
+func (k LineKeymap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Validate},
+		{k.Cancel},
+	}
+}
+
 type LineModel struct {
 	ctx   Context // contexte a exécuter après saisie
 	title string  // titre de la fenêtre modale
 	input textinput.Model
+	help  bt_help.Model
 }
 
 func NewLine(ctx Context, title string, name string, hidden bool) *LineModel {
@@ -17,6 +47,7 @@ func NewLine(ctx Context, title string, name string, hidden bool) *LineModel {
 		ctx:   ctx,
 		title: title,
 		input: textinput.New(),
+		help:  bt_help.New(),
 	}
 
 	m.input.Placeholder = name
@@ -35,20 +66,33 @@ func (m *LineModel) Init() tea.Cmd {
 
 func (m *LineModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if msg.Type == tea.KeyEnter {
+		switch {
+		case key.Matches(msg, DefaultLineKeymap.Validate):
 			return m.Validate()
+		case key.Matches(msg, DefaultLineKeymap.Cancel):
+			return m.Cancel()
 		}
+		m.input, cmd = m.input.Update(msg)
+
+	case tea.WindowSizeMsg:
+		m.help.Width = msg.Width
+
+	default:
+		m.input, cmd = m.input.Update(msg)
+		return m, cmd
 	}
-	m.input, cmd = m.input.Update(msg)
-	return m, cmd
+
+	return m, nil
 }
 
 func (m *LineModel) View() string {
 	return lg.JoinVertical(lg.Left,
 		m.title,
 		m.input.View(),
+		m.help.View(DefaultLineKeymap),
 	)
 }
 
@@ -64,4 +108,8 @@ func (m *LineModel) Validate() (tea.Model, tea.Cmd) {
 		MsgToCmd(ctx),
 	)
 	return m, cmd
+}
+
+func (m *LineModel) Cancel() (tea.Model, tea.Cmd) {
+	return m, MsgToCmd(CloseModalMsg{})
 }

56ab6fca0e637c2e300df7dae98972b02f253d34
Tomtom <tomtom@herbesfolles.org>
2023-05-03 17:24:12 +0200

    cmd plug

---
+++ cmd_plug.go
@@ -0,0 +1,15 @@
+package main
+
+type PlugMsg struct{}
+
+var plug = Cmd{
+	name: "plug",
+	help: "active l'interface neuronale",
+	next: Run(Plug),
+}
+
+func Plug(ctx Context) any {
+	console := ctx.Value("console").(*Console)
+	console.DNI = true
+	return ctx.Result(nil, "interface neuronale directe activée")
+}

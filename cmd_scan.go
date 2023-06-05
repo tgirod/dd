041e3f64d33c40e6983fedc52f4530c0bdedfb0d
Tomtom <tomtom@herbesfolles.org>
2023-06-05 08:54:33 +0200

    cmd SCAN affiche les topics

--- cmd_scan.go
+++ cmd_scan.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+
+	"github.com/asdine/storm/v3/q"
 )
 
 type ScanMsg struct{}
@@ -37,22 +39,48 @@ func Scan(ctx Context) any {
 		return ctx.Error(err)
 	}
 
+	// récupérer les posts
+	topics, err := Find[Post](
+		s.Match(),
+		q.Eq("Parent", 0),
+	)
+	if err != nil {
+		return ctx.Error(err)
+	}
+
 	fmt.Fprintf(tw, "LINKS\n")
 	fmt.Fprintf(tw, "ID\tGroup\tAddress\tDescription\t\n")
 	for _, l := range links {
-		fmt.Fprintf(tw, "%d\t%s\t%s\t%s\t\n", l.ID, l.Group, l.Address, l.Desc)
+		group := "public"
+		if l.Group != "" {
+			group = l.Group
+		}
+		fmt.Fprintf(tw, "%d\t%s\t%s\t%s\t\n", l.ID, group, l.Address, l.Description)
 	}
 	fmt.Fprintln(tw)
 
 	fmt.Fprintf(tw, "REGISTERS\n")
 	fmt.Fprintf(tw, "ID\tGroup\tDescription\tState\t\n")
 	for _, r := range registers {
-		fmt.Fprintf(tw, "%d\t%s\t%s\t%s\t\n", r.ID, r.Group, r.Description, r.State)
+		group := "public"
+		if r.Group != "" {
+			group = r.Group
+		}
+		fmt.Fprintf(tw, "%d\t%s\t%s\t%s\t\n", r.ID, group, r.Description, r.State)
+	}
+	fmt.Fprintln(tw)
+
+	fmt.Fprintf(tw, "FORUM TOPICS\n")
+	fmt.Fprintf(tw, "ID\tGroup\tAuthor\tSubject\tState\t\n")
+	for _, t := range topics {
+		group := "public"
+		if t.Group != "" {
+			group = t.Group
+		}
+		fmt.Fprintf(tw, "%d\t%s\t%s\t%s\t\n", t.ID, group, t.Author, t.Subject)
 	}
 
 	tw.Flush()
-
 	console.StartAlert()
-
 	return ctx.Output(b.String())
 }
